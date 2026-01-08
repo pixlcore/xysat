95a6d3f8d03188817cd9759e9071469609202b3a
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-01-07 19:55:34 -0800

    Changelog Script: Add smarts, tweak formatting.

--- tools/changelog.js
+++ tools/changelog.js
@@ -3,8 +3,19 @@
 // Build xySat CHANGELOG.md file
 
 var fs = require('fs');
+var Path = require('path');
 var cp = require('child_process');
 var Tools = require('pixl-tools');
+
+// make sure we're in the correct dir
+process.chdir( Path.dirname( __dirname ) );
+
+// make sure git sandbox is clean
+var porcelain = cp.execSync('git status --porcelain', { encoding: 'utf8' }).trim();
+if (porcelain.length) {
+	console.error("\nERROR: Git sandbox has local changes.  Please commit these before updating the changelog.\n");
+	process.exit(1);
+}
 
 // get list of tags
 var tags = cp.execSync('git tag --list --sort=version:refname', { encoding: 'utf8' }).trim().split(/\n/).reverse();
@@ -13,6 +24,10 @@ var md = '';
 md += "# xySat Changelog\n";
 
 var last_tag = tags.pop();
+if (!tags.length) {
+	console.error("\nERROR: No tags found after popping first one.  Please push another tag before running changelog.\n");
+	process.exit(1);
+}
 
 tags.forEach( function(tag, idx) {
 	var prev_tag = tags[idx + 1] || last_tag;
@@ -28,17 +43,26 @@ tags.forEach( function(tag, idx) {
 		if (!matches) return;
 		if (matches[4].match(/\b(CHANGELOG)\b/)) return;
 		
-		var url = 'https://github.com/pixlcore/xyops/commit/' + matches[2];
+		var url = 'https://github.com/pixlcore/xysat/commit/' + matches[2];
 		
 		if (first) {
 			md += `> ` + Tools.formatDate( matches[3] + ' 00:00:00', '[mmmm] [mday], [yyyy]' ) + "\n\n";
 			first = false;
 		}
 		
-		md += `- [${matches[1]}](${url}): ` + matches[4] + "\n";
+		md += `- [\`${matches[1]}\`](${url}): ` + matches[4] + "\n";
 	} );
 } );
 
 md += `\n## Version ${last_tag}\n\n> March 17, 2025\n\n- Initial beta release!\n`;
 
 fs.writeFileSync( 'CHANGELOG.md', md );
+
+// make sure log has actually changed
+porcelain = cp.execSync('git status --porcelain', { encoding: 'utf8' }).trim();
+if (!porcelain.length) {
+	console.error("\nWarning: Changelog has not changed since last run.  Skipping actions.\n");
+	process.exit(1);
+}
+
+cp.execSync('git add CHANGELOG.md && git commit -m "Update CHANGELOG" && git push', { stdio: 'inherit' } );
