e99bd7e3b413c1232b515a2d14d41991e0197173
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-01-10 14:43:54 -0800

    Add common PATH locations to container-start.sh

--- container-start.sh
+++ container-start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# add some common path locations
+PATH=$PATH:/usr/bin:/bin:/usr/local/bin:/usr/sbin:/sbin:/usr/local/sbin:$HOME/.local/bin
+
 # check for bootstrap env var, but only on first run
 if [ -n "$XYOPS_setup" ] && [ ! -f "config.json" ]; then
 	echo "Configuring xySat: $XYOPS_setup"
